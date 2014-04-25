8f5e9529ab16a68f249bc99d69f6941041d055a0
chiaming2000 <chiaming2000@gmail.com>
2014-04-25 15:27:44 -0700

    1. Added license files.
    2. Update license headers for java source code and .proto files.

--- kinetic-simulator/src/main/java/com/seagate/kinetic/simulator/heartbeat/HeartbeatProvider.java
+++ kinetic-simulator/src/main/java/com/seagate/kinetic/simulator/heartbeat/HeartbeatProvider.java
@@ -1,11 +1,21 @@
-/** Do NOT modify or remove this copyright and confidentiality notice!
+/**
+ * 
+ * Copyright (C) 2014 Seagate Technology.
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
  *
- * Copyright (c) 2001 - $Date: 2012/06/27 $ Seagate Technology, LLC.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- * The code contained herein is CONFIDENTIAL to Seagate Technology, LLC.
- * Portions are also trade secret. Any use, duplication, derivation, distribution
- * or disclosure of this code, for any reason, not expressly authorized is
- * prohibited. All other rights are expressly reserved by Seagate Technology, LLC.
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
  */
 package com.seagate.kinetic.simulator.heartbeat;
 
