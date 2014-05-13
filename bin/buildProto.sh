89046f3f74adbd41f57b9d0b725ac4d872c83925
lichenchong <lichenchong9@gmail.com>
2014-05-13 16:06:30 -0700

    Modify sync protcol file rules: 1. 'mvn clean compile' will (by default) force auto-sync the kinetic.proto file (based on commit hash in the syncProtoFromRepo.sh script) from kinetic-protocol repo and then compile/generate the Java files.   This requires the internet connection.
    2. Add a flag (-DskipProtoSync) so that the auto-sync can be disabled.
    The maven command line is 'mvn clean compile -DskipProtoSync'.
    
    3. Add a new script to get the latest protocol version from
    kinetic-protocol repo.
    
    4. Update build.setup.readme and kinetic.maven.readme.

--- bin/buildProto.sh
+++ bin/buildProto.sh
@@ -3,11 +3,23 @@ BASE_DIR=`dirname "$0"`/..
 BASE_DIR=`cd "$BASE_DIR"; pwd`
 #echo "BASE_DIR=$BASE_DIR"
 
-echo "Compile protocol file."
-protoc --proto_path=$BASE_DIR/kinetic-common/src/main/java/com/seagate/kinetic/proto --java_out=$BASE_DIR/kinetic-common/src/main/java/ $BASE_DIR/kinetic-common/src/main/java/com/seagate/kinetic/proto/kinetic.proto
-protoc --proto_path=$BASE_DIR/kinetic-common/src/main/java/com/seagate/kinetic/proto --java_out=$BASE_DIR/kinetic-common/src/main/java/ $BASE_DIR/kinetic-common/src/main/java/com/seagate/kinetic/proto/kineticDb.proto
-protoc --proto_path=$BASE_DIR/kinetic-common/src/main/java/com/seagate/kinetic/proto --java_out=$BASE_DIR/kinetic-common/src/main/java/ $BASE_DIR/kinetic-common/src/main/java/com/seagate/kinetic/proto/kineticIo.proto
+PROTO_DIR=$BASE_DIR/kinetic-common/src/main/java/com/seagate/kinetic/proto/
+PROTO_FILE=$BASE_DIR/kinetic-common/src/main/java/com/seagate/kinetic/proto/kinetic.proto
+PROTO_COMPILE_DIR=$BASE_DIR/kinetic-common/src/main/java/
 
-echo "Compile finished."
+function compileProto(){
+    echo "Compile protocol file: $PROTO_FILE."
+    protoc --proto_path=$PROTO_DIR --java_out=$PROTO_COMPILE_DIR $PROTO_FILE
+
+    echo "Compile protocol file: $PROTO_DIR/kineticDb.proto."
+    protoc --proto_path=$PROTO_DIR --java_out=$PROTO_COMPILE_DIR $PROTO_DIR/kineticDb.proto
+
+    echo "Compile protocol file: $PROTO_DIR/kineticIo.proto."
+    protoc --proto_path=$PROTO_DIR --java_out=$PROTO_COMPILE_DIR $PROTO_DIR/kineticIo.proto
+
+    echo "Compile finished."
+}
+
+compileProto
 
 exit 0
--- bin/forceSyncLatestProtoFromRepo.sh
+++ bin/forceSyncLatestProtoFromRepo.sh
@@ -6,7 +6,6 @@ BASE_DIR=`cd "$BASE_DIR"; pwd`
 PROTO_REPO_URL=https://github.com/Seagate/Kinetic-Protocol.git
 PROTO_FILE=$BASE_DIR/kinetic-common/src/main/java/com/seagate/kinetic/proto/kinetic.proto
 CLONE_DIR=$BASE_DIR/bin/Kinetic-ProtocoL
-PROTO_COMMIT_HASH=bdfe223648ca13df2db0dabe328980025b8278e4
 
 function syncFromProtoRepo(){
     if [ -d "$CLONE_DIR" ]; then
@@ -14,10 +13,8 @@ function syncFromProtoRepo(){
     fi
 
     if [ $# -eq 0 ]; then
-        echo "Clone protocol file from github, the commit hash is: $PROTO_COMMIT_HASH"
+        echo "Clone the latest protocol file from github:"
         git clone $PROTO_REPO_URL $CLONE_DIR
-        cd $CLONE_DIR
-        git checkout $PROTO_COMMIT_HASH
     fi
 
     cp $CLONE_DIR/kinetic.proto $PROTO_FILE
