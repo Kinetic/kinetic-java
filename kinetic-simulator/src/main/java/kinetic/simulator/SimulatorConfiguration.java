f4e39a66b206ba6201f3c433a747caeebebb7705
chiaming2000 <chiaming2000@gmail.com>
2014-07-14 11:17:01 -0700

    Sync kinetic.proto file to release 2.0.5.

--- kinetic-simulator/src/main/java/kinetic/simulator/SimulatorConfiguration.java
+++ kinetic-simulator/src/main/java/kinetic/simulator/SimulatorConfiguration.java
@@ -201,12 +201,12 @@ public class SimulatorConfiguration extends Properties {
     /**
      * current supported protocol version defined at kinetic-protocol repository.
      */
-    public static final String PROTOCOL_VERSION = "2.0.4";
+    public static final String PROTOCOL_VERSION = "2.0.5";
     
     /**
      * current supported protocol source commit hash value obtained from kinetic-protocol repository.
      */
-    public static final String PROTOCOL_SOURCE_HASH = "337cdcef1c9d64b3cbfa7079fb304b5459bc335f";
+    public static final String PROTOCOL_SOURCE_HASH = "80b18b440c05c5994cb1bd555f680f8f62bb0337";
     
     /**
      * heart beat provider.
