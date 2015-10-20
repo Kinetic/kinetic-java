b9ee01cbc79a14d65cdfd25937e3616617c3a671
chiaming2000 <chiaming2000@gmail.com>
2015-10-19 17:47:55 -0700

    Update program headers and LICENSE file to use Mozilla Public License, version 2.0.

--- kinetic-simulator/src/main/java/com/seagate/kinetic/simulator/persist/leveldb/LdbBatchOperation.java
+++ kinetic-simulator/src/main/java/com/seagate/kinetic/simulator/persist/leveldb/LdbBatchOperation.java
@@ -1,3 +1,19 @@
+/**
+ * Copyright 2013-2015 Seagate Technology LLC.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla
+ * Public License, v. 2.0. If a copy of the MPL was not
+ * distributed with this file, You can obtain one at
+ * https://mozilla.org/MP:/2.0/.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but is provided AS-IS, WITHOUT ANY WARRANTY; including without 
+ * the implied warranty of MERCHANTABILITY, NON-INFRINGEMENT or 
+ * FITNESS FOR A PARTICULAR PURPOSE. See the Mozilla Public 
+ * License for more details.
+ *
+ * See www.openkinetic.org for more project information
+ */
 package com.seagate.kinetic.simulator.persist.leveldb;
 
 import java.io.IOException;
